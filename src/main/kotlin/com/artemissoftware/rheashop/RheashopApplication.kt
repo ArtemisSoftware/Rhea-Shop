70c9164f9e665413cc1f0b883c6f2010d20b3125
gustavo <gustavo.isaac@gmail.com>
2025-01-09 18:18:51 +0000

    Initial commit

---
+++ src/main/kotlin/com/artemissoftware/rheashop/RheashopApplication.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.rheashop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RheashopApplication
+
+fun main(args: Array<String>) {
+	runApplication<RheashopApplication>(*args)
+}
---
+++ src/test/kotlin/com/artemissoftware/rheashop/RheashopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.artemissoftware.rheashop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RheashopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
