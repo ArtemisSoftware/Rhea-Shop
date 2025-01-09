af219c91bae8ba0f8864d7392ec9ff9675ff71c4
gustavo <gustavo.isaac@gmail.com>
2025-01-09 18:32:21 +0000

    ImageEntity

---
+++ src/main/kotlin/com/artemissoftware/rheashop/data/database/entities/ImageEntity.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.rheashop.data.database.entities
+
+import jakarta.persistence.*
+import java.sql.Blob
+
+
+class ImageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
+    var fileName: String = ""
+    var fileType: String = ""
+
+    @Lob
+    var image: Blob? = null
+    var downloadUrl: String = ""
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    var product: ProductEntity? = null
+}
