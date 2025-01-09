9de497316af1f9f582ff7c3518500e47fbe68ecc
gustavo <gustavo.isaac@gmail.com>
2025-01-09 18:32:13 +0000

    CategoryEntity

---
+++ src/main/kotlin/com/artemissoftware/rheashop/data/database/entities/CategoryEntity.kt
@@ -0,0 +1,14 @@
+package com.artemissoftware.rheashop.data.database.entities
+import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.persistence.*
+
+class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
+    var name: String = ""
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    var products: List<ProductEntity> = emptyList()
+}
