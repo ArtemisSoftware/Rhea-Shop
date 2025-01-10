af66f38dc529cfee259fe761ecc0f67e35c25965
gustavo <gustavo.isaac@gmail.com>
2025-01-10 09:59:02 +0000

    ProductRepository

---
+++ src/main/kotlin/com/artemissoftware/rheashop/repository/ProductRepository.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.rheashop.repository
+
+import com.artemissoftware.rheashop.data.database.entities.ProductEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProductRepository: JpaRepository<ProductEntity, Long> {
+    fun findByCategoryName(category: String?): List<ProductEntity>
+    fun findByBrand(brand: String?): List<ProductEntity>
+    fun findByCategoryNameAndBrand(category: String, brand: String): List<ProductEntity>
+    fun findByName(name: String): List<ProductEntity>
+    fun findByBrandAndName(brand: String, name: String): List<ProductEntity>
+    fun countByBrandAndName(brand: String, name: String): Long
+    fun existsByNameAndBrand(name: String, brand: String): Boolean
+}
