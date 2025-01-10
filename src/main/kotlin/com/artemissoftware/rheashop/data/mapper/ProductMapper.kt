14edb1a218d9f7b4bb1da43a35eae60a50f3310e
gustavo <gustavo.isaac@gmail.com>
2025-01-10 10:57:23 +0000

    ProductMapper

---
+++ src/main/kotlin/com/artemissoftware/rheashop/data/mapper/ProductMapper.kt
@@ -0,0 +1,29 @@
+package com.artemissoftware.rheashop.data.mapper
+
+import com.artemissoftware.rheashop.data.database.entities.CategoryEntity
+import com.artemissoftware.rheashop.data.database.entities.ProductEntity
+import com.artemissoftware.rheashop.data.network.request.AddProductRequest
+import com.artemissoftware.rheashop.data.network.request.UpdateProductRequest
+
+fun AddProductRequest.toEntity(category: CategoryEntity): ProductEntity{
+    return ProductEntity(
+        name = name,
+        brand = brand,
+        price = price,
+        inventory = inventory,
+        description = description,
+        category = category
+    )
+}
+
+fun UpdateProductRequest.toEntity(existingProduct: ProductEntity, category: CategoryEntity): ProductEntity {
+    existingProduct.name = name
+    existingProduct.brand = brand
+    existingProduct.price = price
+    existingProduct.inventory = inventory
+    existingProduct.description = description
+
+    val category: Category = categoryRepository.findByName(request.getCategory().getName())
+    existingProduct.setCategory(category)
+    return existingProduct
+}
--- src/main/kotlin/com/artemissoftware/rheashop/service/ProductService.kt
+++ src/main/kotlin/com/artemissoftware/rheashop/service/ProductService.kt
@@ -1,13 +1,50 @@
 package com.artemissoftware.rheashop.service
 
+import com.artemissoftware.rheashop.data.database.entities.CategoryEntity
+import com.artemissoftware.rheashop.data.database.entities.ProductEntity
+import com.artemissoftware.rheashop.data.mapper.toEntity
+import com.artemissoftware.rheashop.data.network.request.AddProductRequest
+import com.artemissoftware.rheashop.data.network.request.UpdateProductRequest
 import com.artemissoftware.rheashop.exception.ProductNotFoundException
+import com.artemissoftware.rheashop.repository.CategoryRepository
 import com.artemissoftware.rheashop.repository.ProductRepository
 import org.springframework.stereotype.Service
+import java.util.function.Supplier
+
 
 @Service
-class ProductService(private val productRepository: ProductRepository) {
+class ProductService(
+    private val productRepository: ProductRepository,
+    private val categoryRepository: CategoryRepository
+) {
 
     private fun productExists(name: String, brand: String) = productRepository.existsByNameAndBrand(name, brand)
+
+    fun addProduct(request: AddProductRequest): ProductEntity {
+//        if (productExists(request.name, request.brand)) {
+//            throw AlreadyExistsException(request.brand + " " + request.name + " already exists, you may update this product instead!")
+//        }
+
+
+        val category = categoryRepository.findByName(request.category.name)
+            ?: categoryRepository.save(CategoryEntity(request.category.name))
+
+        request.category = category
+        return productRepository.save(request.toEntity(category))
+    }
+
+    fun updateProduct(request: UpdateProductRequest, productId: Long): ProductEntity {
+        return productRepository.findById(productId)
+            .map { existingProduct ->
+                categoryRepository.findByName(request.category.name)?.let {
+                    request.toEntity(existingProduct = existingProduct, category = it)
+                } ?: run {
+                    null
+                }
+            }
+            .map { entity -> entity?.let { productRepository.save(it) } }
+            .orElseThrow { ProductNotFoundException("Product not found!") }
+    }
 
     fun deleteProductById(id: Long) = with(productRepository) {
         findById(id)
