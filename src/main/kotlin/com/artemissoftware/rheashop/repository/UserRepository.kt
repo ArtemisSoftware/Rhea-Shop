720213ae837d4849e55f4f93a3cdf31e7a010f87
gustavo <gustavo.isaac@gmail.com>
2025-01-20 16:57:37 +0000

    UserDto

--- src/main/kotlin/com/artemissoftware/rheashop/repository/UserRepository.kt
+++ src/main/kotlin/com/artemissoftware/rheashop/repository/UserRepository.kt
@@ -2,8 +2,9 @@ package com.artemissoftware.rheashop.repository
 
 import com.artemissoftware.rheashop.data.database.entities.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-
+@Repository
 interface UserRepository : JpaRepository<UserEntity, Long> {
     fun existsByEmail(email: String): Boolean
 
