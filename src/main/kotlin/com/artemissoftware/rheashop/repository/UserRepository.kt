557c40d0d62e85c586d8d782b2017d8c6f961cc7
gustavo <gustavo.isaac@gmail.com>
2025-01-20 16:22:21 +0000

    UserRepository

---
+++ src/main/kotlin/com/artemissoftware/rheashop/repository/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.rheashop.repository
+
+import com.artemissoftware.rheashop.data.database.entities.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+
+interface UserRepository : JpaRepository<UserEntity, Long> {
+    fun existsByEmail(email: String): Boolean
+
+    fun findByEmail(email: String): UserEntity
+}
