8c9a514273b79c2e80349fd5744543ed414a8405
gustavo <gustavo.isaac@gmail.com>
2025-01-21 17:16:12 +0000

    ShopUserDetailsService

--- src/main/kotlin/com/artemissoftware/rheashop/security/user/ShopUserDetailsService.kt
+++ src/main/kotlin/com/artemissoftware/rheashop/security/user/ShopUserDetailsService.kt
@@ -1,19 +1,26 @@
 package com.artemissoftware.rheashop.security.user
 
+import com.artemissoftware.rheashop.repository.UserRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
 
-//@Service
-//class ShopUserDetailsService : UserDetailsService {
-//    private val userRepository: UserRepository? = null
-//
-//    @Throws(UsernameNotFoundException::class)
-//    override fun loadUserByUsername(email: String?): UserDetails {
-//        val user: User = Optional.ofNullable(userRepository.findByEmail(email))
-//            .orElseThrow { UsernameNotFoundException("User not found!") }
-//        return ShopUserDetails.buildUserDetails(user)
-//    }
-//}
+@Service
+class ShopUserDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+
+    override fun loadUserByUsername(email: String?): UserDetails {
+        email?.let {
+            userRepository.findByEmail(it)?.let { user ->
+                return ShopUserDetails.buildUserDetails(user)
+            } ?: run {
+                throw UsernameNotFoundException("User not found!")
+            }
+        } ?:run {
+            throw UsernameNotFoundException("Email is not valid")
+        }
+    }
+}
