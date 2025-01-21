17982352ec6e8fc7ae3d49ae14bd2956f75c9588
gustavo <gustavo.isaac@gmail.com>
2025-01-21 14:40:21 +0000

    UserService

---
+++ src/main/kotlin/com/artemissoftware/rheashop/service/UserService.kt
@@ -0,0 +1,61 @@
+package com.artemissoftware.rheashop.service
+
+import com.artemissoftware.rheashop.data.database.entities.UserEntity
+import com.artemissoftware.rheashop.data.network.dto.UserDto
+import com.artemissoftware.rheashop.data.network.request.CreateUserRequest
+import com.artemissoftware.rheashop.data.network.request.UserUpdateRequest
+import com.artemissoftware.rheashop.exception.AlreadyExistsException
+import com.artemissoftware.rheashop.exception.ResourceNotFoundException
+import com.artemissoftware.rheashop.repository.UserRepository
+import org.springframework.stereotype.Service
+
+
+@Service
+class UserService(
+    private val userRepository: UserRepository
+) {
+
+    fun getUserById(userId: Long): UserEntity {
+        return userRepository.findById(userId)
+            .orElseThrow { ResourceNotFoundException("User not found!") }
+    }
+
+    fun createUser(request: CreateUserRequest): UserEntity {
+
+        if(userRepository.existsByEmail(request.email)){
+            throw AlreadyExistsException("Oops!" + request.email + " already exists!")
+        }
+        else {
+            val user = UserEntity()
+            user.email = request.email
+            user.password = "1234"//passwordEncoder.encode(request.password)
+            user.firstName = request.firstName
+            user.lastName = request.lastName
+            return userRepository.save(user)
+        }
+    }
+
+    fun updateUser(request: UserUpdateRequest, userId: Long): UserEntity {
+        return userRepository.findById(userId)
+            .map {
+                it.firstName = request.firstName
+                it.lastName = request.lastName
+                userRepository.save(it)
+            }
+            .orElseThrow { ResourceNotFoundException("User not found!") }
+    }
+
+    fun deleteUser(userId: Long) {
+        userRepository.findById(userId)
+            .ifPresentOrElse(
+                { userRepository.delete(it) },
+                { throw ResourceNotFoundException("User not found!") }
+            )
+    }
+
+//    override fun getAuthenticatedUser(): User {
+//        val authentication: Authentication = SecurityContextHolder.getContext().getAuthentication()
+//        val email: String = authentication.getName()
+//        return userRepository!!.findByEmail(email)
+//    }
+}
