16aed622fe3bf81052ace65ab1a547b4a234b4e4
gustavo <gustavo.isaac@gmail.com>
2025-01-21 17:33:18 +0000

    Jwt dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,11 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
