0bd079c69a5e6cc1767c701438d206298ff1cbf5
gustavo <gustavo.isaac@gmail.com>
2025-01-28 12:21:45 +0000

    JwtAuthEntryPoint

---
+++ src/main/kotlin/com/artemissoftware/rheashop/security/jwt/JwtAuthEntryPoint.kt
@@ -0,0 +1,37 @@
+package com.artemissoftware.rheashop.security.jwt
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+import java.io.IOException
+
+@Component
+class JwtAuthEntryPoint : AuthenticationEntryPoint {
+
+    @Throws(IOException::class, ServletException::class)
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException
+    ) {
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.status = HttpServletResponse.SC_UNAUTHORIZED
+
+        val body: MutableMap<String, Any> = HashMap()
+        //
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body["error"] = "Unauthorized"
+        body["message"] = "You may login and try again!"
+
+        /** body.put("path", request.getServletPath()); */
+        body["path"] = request.servletPath
+
+        val mapper = ObjectMapper()
+        mapper.writeValue(response.outputStream, body)
+    }
+}
